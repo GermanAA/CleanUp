b5acfa3dc839688107294718eddb5864a941bc03
Germán <armendariz.german@gmail.com>
2025-04-01 21:09:01 -0600

    Agregar Fuente Poppind - Color

--- views/login.view.php
+++ views/login.view.php
@@ -8,7 +8,9 @@
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<!--<script src="js/jquery-3.5.1.js"></script>-->
-	<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
 	<script src="https://use.fontawesome.com/603f27fb7e.js"></script>
 	<link rel="stylesheet" type="text/css" href="css\estilos.css">
 	<script type="text/javascript" src="js/index.js"></script>
